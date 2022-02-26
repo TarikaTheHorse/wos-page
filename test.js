67833ea1867e6e677f2121e50ab665649fd5a94a
TarikaTheHorse <violette.and.john@gmail.com>
2022-02-25 20:16:34 -0800

    up

--- test.js
+++ test.js
@@ -1,16 +1,30 @@
-function keepClosed() {
-    // Declare all variables
-    var i, tabcontent;
-  
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+'use strict';
+
+function Tabs() {
+  var bindAll = function() {
+    var menuElements = document.querySelectorAll('[data-tab]');
+    for(var i = 0; i < menuElements.length ; i++) {
+      menuElements[i].addEventListener('click', change, false);
     }
+  }
+
+  var clear = function() {
+    var menuElements = document.querySelectorAll('[data-tab]');
+    for(var i = 0; i < menuElements.length ; i++) {
+      menuElements[i].classList.remove('active');
+      var id = menuElements[i].getAttribute('data-tab');
+      document.getElementById(id).classList.remove('active');
+    }
+  }
+
+  var change = function(e) {
+    clear();
+    e.target.classList.add('active');
+    var id = e.currentTarget.getAttribute('data-tab');
+    document.getElementById(id).classList.add('active');
+  }
+
+  bindAll();
 }
-function tabOpen(cityName) {
-    document.getElementById(cityName).style.display = "block";
-}
-document.getElementById("l").onclick = function() {tabOpen('London')};
-document.getElementById("p").onclick = function() {tabOpen('Paris')};
-document.getElementById("t").onclick = function() {tabOpen('Tokyo')};
+
+var connectTabs = new Tabs();
